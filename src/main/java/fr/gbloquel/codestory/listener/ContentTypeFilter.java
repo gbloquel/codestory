f6189732c68ddb8448dd9c0154a41c4264bb1419
greg <greg@greg.fr>
2013-01-28 21:39:00 +0100

    Log jajascript request in a file temporary

--- src/main/java/fr/gbloquel/codestory/listener/ContentTypeFilter.java
+++ src/main/java/fr/gbloquel/codestory/listener/ContentTypeFilter.java
@@ -1,10 +1,20 @@
 package fr.gbloquel.codestory.listener;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+import fr.gbloquel.codestory.services.JajaScriptResource;
 
 /**
  * This class update the content Type FROM application/x-www-form-urlencoded to
@@ -15,6 +25,9 @@ import com.sun.jersey.spi.container.ContainerRequestFilter;
  */
 public class ContentTypeFilter implements ContainerRequestFilter {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(ContentTypeFilter.class);
+	
     @Override
     public ContainerRequest filter(ContainerRequest request) {
 
@@ -22,6 +35,15 @@ public class ContentTypeFilter implements ContainerRequestFilter {
 
             request.getRequestHeaders().remove(HttpHeaders.CONTENT_TYPE);
             request.getRequestHeaders().putSingle(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+            
+            
+            try {
+				IOUtils.copy(request.getEntityInputStream(), new FileOutputStream("request.json"));
+			} catch (FileNotFoundException e) {
+				logger.warn("The file is not found", e);
+			} catch (IOException e) {
+				logger.warn("I/O exception during the copy.", e);
+			}
 
         }
         return request;
